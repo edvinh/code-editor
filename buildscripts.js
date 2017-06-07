aee9784e7754bd0f8967b5a63d3a711d8eee5575
linde12 <lindeoscar@gmail.com>
2017-06-07 22:54:32 +0200

    fix: platform-independent paths

--- buildscripts.js
+++ buildscripts.js
@@ -1,22 +1,30 @@
+const path = require('path')
+const filepath = name => path.join(__dirname, `tmp/${filename}`)
+
 const java = (filename) => {
-  return `javac tmp\\${filename}.java & java -cp tmp ${filename}`
+  return `javac ${filepath(filename)}.java & java -cp tmp ${filename}`
 }
 
 const js = (filename) => {
-  return `node tmp\\${filename}.js`
+  return `node ${filepath(filename)}.js`
 }
 
 const python = (filename) => {
-  return `py tmp\\${filename}.py`
+  return `py ${filepath(filename)}.py`
 }
 
 const haskell = (filename) => {
-  return `ghc --make tmp\\${filename}.hs & tmp\\${filename}.exe`
+  return `ghc --make ${filepath(filename)}.hs & ${filepath(filename)}.exe`
+}
+
+const go = (filename) => {
+  return `go run ${filepath(filename)}`
 }
 
 module.exports = {
   java,
   js,
+  go,
   python,
   haskell,
 }
